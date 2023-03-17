ca7bacfc747ccfcc01abb82a67e655d50550aa28
opilane <opilane@HP-CZC2349HTF>
2023-03-17 08:41:22 +0200

    wrking

--- HoroskoopJaAjaplaan/HoroskoopJaAjaplaan/MainPage.xaml.cs
+++ HoroskoopJaAjaplaan/HoroskoopJaAjaplaan/MainPage.xaml.cs
@@ -5,14 +5,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HoroskoopJaAjaplaan
 {
     public partial class MainPage : ContentPage
     {
+        List<ContentPage> cntPages = new List<ContentPage>() { new Horoskop(), new AiaPlaan(), new MinuOma()};
+        List<string> tekstid = new List<string> { "Horoskoop", "Aiaplaan", "Minu Oma" };
         public MainPage()
         {
-            InitializeComponent();
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Gray,
+            };
+            for (int i = 0; i < cntPages.Count; i++)
+            {
+                Button btn = new Button
+                {
+                    Text = tekstid[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.White,
+                    TextColor = Color.Black,
+                };
+                btn.Clicked += NavigFunkt;
+                st.Children.Add(btn);
+            }
+            Content = st;
+        }
+
+        private async void NavigFunkt(object sender, EventArgs e)
+        {
+            Button bSender = (Button)sender;
+            await Navigation.PushAsync(cntPages[bSender.TabIndex]);
         }
     }
 }
